5332f11ceca303c1127a79e7c4a296a1db6410a2
Kirsty Rose <krhome21@gmail.com>
2014-06-04 23:33:43 +0100

    initial commit
    commit feed.js

---
+++ feed.js
@@ -0,0 +1,27 @@
+//facebook feed
+
+
+  //create vars
+	var user = 'username';
+	var access_token = 'accesstoken';
+	var secret = 'yoursecret';
+	
+	//build url
+	var json = 'https://graph.facebook.com/'+user+'/feed?access_token='+access_token+'|'+secret;
+
+  //fetch data
+	$.getJSON(json, function(feed) {
+
+		console.log('feed', feed);
+
+    //loop thorugh data
+		for(var i = 0; i < 1; i++) {
+			
+			var postData = feed.data[i];
+      
+      //append data to feed
+			$('.feed').append(feed.data[i].message);
+
+		}
+
+	})
