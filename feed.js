25f2f90fb8f1b24ad67ac6b114b6c27b93cb1d14
Kirsty Rose <krhome21@gmail.com>
2014-06-04 23:35:04 +0100

    tidied code

--- feed.js
+++ feed.js
@@ -1,27 +1,27 @@
 //facebook feed
 
 
-  //create vars
-	var user = 'username';
-	var access_token = 'accesstoken';
-	var secret = 'yoursecret';
+//create vars
+var user = 'username';
+var access_token = 'accesstoken';
+var secret = 'yoursecret';
 	
-	//build url
-	var json = 'https://graph.facebook.com/'+user+'/feed?access_token='+access_token+'|'+secret;
+//build url
+var json = 'https://graph.facebook.com/'+user+'/feed?access_token='+access_token+'|'+secret;
 
-  //fetch data
-	$.getJSON(json, function(feed) {
+//fetch data
+$.getJSON(json, function(feed) {
 
-		console.log('feed', feed);
+	console.log('feed', feed);
 
-    //loop thorugh data
-		for(var i = 0; i < 1; i++) {
+	//loop thorugh data
+	for(var i = 0; i < 1; i++) {
 			
-			var postData = feed.data[i];
+		var postData = feed.data[i];
       
-      //append data to feed
-			$('.feed').append(feed.data[i].message);
+      		//append data to feed
+		$('.feed').append(feed.data[i].message);
 
-		}
+	}
 
-	})
+})
