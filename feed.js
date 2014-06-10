7080a12b1d1269e3fa86bca439c2cdfba262173e
Kirsty Rose <krhome21@gmail.com>
2014-06-10 23:15:49 +0100

    Update feed.js

--- feed.js
+++ feed.js
@@ -1,27 +1,38 @@
-//facebook feed
+// facebook feed class
+var Facebook = function(config) {
 
+	// build the url
+	var feedUrl = 'https://graph.facebook.com/'+config.user+'/feed?access_token='+config.access_token+'|'+config.secret;
 
-//create vars
-var user = 'username';
-var access_token = 'accesstoken';
-var secret = 'yoursecret';
-	
-//build url
-var json = 'https://graph.facebook.com/'+user+'/feed?access_token='+access_token+'|'+secret;
+	// declares a function which requests json from facebook
+	var getFacebookFeed = function(callback) {
+		// request the data and call the callback
+		$.getJSON(feedUrl, callback);
+	};
+	return {
+		getFeed: getFacebookFeed
+	};
+}
 
-//fetch data
-$.getJSON(json, function(feed) {
+//object
+var config = {
+	// key/value
+	user: 'username',
+	access_token: 'accesstoken',
+	secret: 'yoursecret'
+};
 
-	console.log('feed', feed);
-
-	//loop thorugh data
+var feed1 = new Facebook(config);
+feed1.getFeed(function(feed) {
+	//loop through data
 	for(var i = 0; i < 1; i++) {
 			
 		var postData = feed.data[i];
       
-      		//append data to feed
+      	//append data to feed
 		$('.feed').append(feed.data[i].message);
 
 	}
+});
 
-})
+feed.update()
