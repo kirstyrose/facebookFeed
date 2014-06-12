65bbe6ece3a0f82fd2cddc844c0f21b47c46ea7d
Kirsty Rose <krhome21@gmail.com>
2014-06-12 10:04:39 +0100

    Made code reusable

--- feed.js
+++ feed.js
@@ -1,36 +1,75 @@
 // facebook feed class
 var Facebook = function(config) {
 
-	// build the url
-	var feedUrl = 'https://graph.facebook.com/'+config.user+'/feed?access_token='+config.access_token+'|'+config.secret;
+	var self = this;
 
-	// declares a function which requests json from facebook
-	var getFacebookFeed = function(callback) {
-		// request the data and call the callback
-		$.getJSON(feedUrl, callback);
-	};
-	return {
-		getFeed: getFacebookFeed
-	};
-}
+	var defaults = {
 
-//object
-var config = {
-	// key/value
-	user: 'username',
-	access_token: 'accesstoken',
-	secret: 'yoursecret'
-};
-
-var feed1 = new Facebook(config);
-feed1.getFeed(function(feed) {
-	//loop through data
-	for(var i = 0; i < 1; i++) {
-			
-		var postData = feed.data[i];
-      
-      		//append data to feed
-		$('.feed').append(feed.data[i].message);
+		user: null,
+		access_token: null,
+		secret: null,
+		count: 1
 
 	}
+
+	//extend defaults to contfig
+	var settings = $.extend(defaults, config);
+
+	//fallback for unset property
+	for(property in settings) {
+
+		if(settings[property] == null) console.log('Please fill in '+property);
+	}
+
+	// build the url
+	var feedUrl = 'https://graph.facebook.com/'+settings.user+'/feed?access_token='+settings.access_token+'|'+settings.secret;
+
+
+	var privateMethod = {
+
+		append: function(){
+			
+			$.getJSON(feedUrl, function(feed){
+
+				for(var i = 0; i < settings.count; i++) {
+						
+					var postData = feed.data[i];
+			      
+			      	//append data to feed
+					$('.feed').append('<li>' + postData.message + '</li>');
+
+				}
+
+			});
+
+		}, 
+
+		init: function(){
+
+			self.publicMethod['alert']()
+			privateMethod.append()
+
+		}
+	}
+
+	self.publicMethod = {
+
+		alert: function(){
+
+			alert('such structure. many functions. wow');
+
+		}
+
+	}
+
+	privateMethod.init()
+
+}
+
+//new instances
+var feed1 = new Facebook({
+	user: 'propellercomms',
+	access_token: '1418975825038822',
+	secret: 'c0383c010531c0f19a1ae48d13a00634',
+	count: 8
 });
